ae882d062f5eb8ae1c7a2fe12ba12725a3aef5ea
drewg290 <Drewgloria1@gmail.com>
2025-01-16 12:36:47 -0800

    fixed the javascript

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var code1;
-code1 = 7 + 3;
+let code1 = 7 + 3;
 
-var code2;
-code2 = 140 / 2 - 30;
+const code2 = 140 / 2 - 30;
 
-var code3;
-code3 = 169 % 100;
+const code3 = 169 % 100;
 
-var message = " The vault has been secured. The combination is:";
+const message = " The vault has been secured. The combination is:";
 
-var codeA = code1 + "-" + code2 + "-" + code3;
-var codeB = code1 + "-" + code2 + "-" + code3;
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = code1 + "-" + code2 + "-" + code3;
 
 console.log(message);
 console.log(codeA);
